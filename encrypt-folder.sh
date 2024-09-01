d7839f7493132a35bd64d5620e568af559e790ee
pman <pman-$-432894328923@gmail.com>
2024-09-01 23:00:51 +0200

    ...

--- encrypt-folder.sh
+++ encrypt-folder.sh
@@ -14,6 +14,6 @@ age-keygen >> ~/.config/age/keys.txt
 
 pubkey=$(grep -oP '(?<=public key: ).*' ~/.config/age/keys.txt | tail -n 1)
 
-tar cvz "./$dir" | age -r "$pubkey" --armor > "$dir".tar.gz.age
+tar cvz "./$dir" | age -r "$pubkey" --armor > "./${dir}.tar.gz.age"
 
 echo "encryption complete of ./$dir to ./${dir}.tar.gz.age... safe to remove ./$dir"
